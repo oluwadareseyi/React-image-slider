674be832d6f76ba5ec462496ddefb460cd8ef4a0
oluwadareseyi <oluwadareseyii@gmail.com>
2020-05-13 17:06:50 +0100

    added dynamic width

---
+++ src/components/Slide.js
@@ -0,0 +1,22 @@
+/** @jsx jsx */
+import React from "react";
+import { css, jsx } from "@emotion/core";
+/**
+ * @function Slide
+ * @param  {Object} {content} - Destructured to get the image URL and use it as the background.
+ * @returns {JSX.Element}
+ */
+const Slide = ({ content }) => (
+  <div
+    css={css`
+      height: 100%;
+      width: 100%;
+      background-image: url('${content}');
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+    `}
+  />
+);
+
+export default Slide;
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -2,6 +2,7 @@
 import React, { useState } from "react";
 import { css, jsx } from "@emotion/core";
 import SliderContent from "./SliderContent";
+import Slide from "./Slide";
 
 /**
  * @function Slider - The main component
@@ -9,12 +10,18 @@ import SliderContent from "./SliderContent";
  */
 
 const Slider = () => {
+  const images = [
+    "https://images.unsplash.com/photo-1449034446853-66c86144b0ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2100&q=80",
+    "https://images.unsplash.com/photo-1470341223622-1019832be824?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2288&q=80",
+    "https://images.unsplash.com/photo-1448630360428-65456885c650?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2094&q=80",
+    "https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80",
+  ];
   // We get the width of the current viewport
   const getWidth = () => window.innerWidth;
 
   // State - Object that stores the current translate value and the transition timing.
   const [state, setState] = useState({
-    translate: 0,
+    translate: getWidth(),
     transition: 0.45,
   });
 
@@ -25,8 +32,13 @@ const Slider = () => {
       <SliderContent
         translate={translate}
         transition={transition}
-        width={getWidth()}
-      ></SliderContent>
+        // multiply the current width by the number of images in the array so one image takes the full width of whatever device we're using.
+        width={getWidth() * images.length}
+      >
+        {images.map((img, i) => (
+          <Slide key={i} content={img} />
+        ))}
+      </SliderContent>
     </div>
   );
 };
